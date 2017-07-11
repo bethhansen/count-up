0d4f8be33455ac6cc4823d0aa9728b57e82a8f72
Beth Hansen and Kate Mashokha <kate@>
2017-07-11 10:46:29 -0700

    add count up

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 
   $("form#grocery").submit(function(event){
+    alert("hi");
     var items = $("#item").val().toUpperCase().split(",");
     var alphOrderArray = items.sort();
     alphOrderArray.forEach(function(alpha) {
